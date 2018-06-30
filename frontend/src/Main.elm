5cd48547d8c16879b20296daff5a937d8f0ed5f2
Pascal Widdershoven <hello@pascalw.me>
2018-06-30 16:34:39 +0200

    wip

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,37 +1,70 @@
 module Main exposing (..)
 
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html exposing (Html, text, div, h1, img, ul, li)
+import Time exposing (Time)
+import Date
+import Task
 
 
 ---- MODEL ----
 
-type alias NoteBook = {
-    name: String,
-    notes: List Note
-}
 
-type alias Note = {
-    contentBlocks: List Content,
-    tags: List Tag
-}
+type alias NoteBook =
+    { name : String
+    , notes : List Note
+    , createdAt : Time
+    }
 
-type Content = MarkdownContent { 
-    text: String
-} | CodeContent {
-    text: String,
-    language: String
-}
 
-type alias Tag = String
+type alias Note =
+    { title : String
+    , createdAt : Time
+    , updatedAt : Time
+    , contentBlocks : List Content
+    , tags : List Tag
+    }
+
+
+type Content
+    = MarkdownContent
+        { text : String
+        }
+    | CodeContent
+        { text : String
+        , language : String
+        }
+
+
+type alias Tag =
+    String
+
 
 type alias Model =
-    {}
+    { notebooks : Maybe (List NoteBook)
+    }
+
+
+buildNote : String -> Time -> String -> Note
+buildNote title createdAt contentString =
+    let
+        content =
+            MarkdownContent { text = contentString }
+    in
+        Note title createdAt createdAt [ content ] []
+
+
+buildSeedNoteBook : Time -> NoteBook
+buildSeedNoteBook createdAt =
+    let
+        note =
+            buildNote "My first note" createdAt "Hello *world*!"
+    in
+        NoteBook "Pascal" [ note ] createdAt
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( Model Nothing, Task.perform CurrentTime Time.now )
 
 
 
@@ -39,23 +72,63 @@ init =
 
 
 type Msg
-    = NoOp
+    = CurrentTime Time
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        CurrentTime time ->
+            let
+                noteBook =
+                    buildSeedNoteBook time
+            in
+                ( { model | notebooks = (Just [ noteBook ]) }, Cmd.none )
 
 
 
 ---- VIEW ----
 
 
+renderNote : Note -> Html Msg
+renderNote note =
+    li [] [ text note.title, text (note.createdAt |> Date.fromTime |> toString) ]
+
+
+renderNotes : List Note -> Html Msg
+renderNotes notes =
+    ul [] (List.map renderNote notes)
+
+
+renderNoteBook : NoteBook -> Html Msg
+renderNoteBook noteBook =
+    li []
+        [ (text noteBook.name)
+        , renderNotes noteBook.notes
+        ]
+
+
+renderNoteBooks : Maybe (List NoteBook) -> Html Msg
+renderNoteBooks noteBooks =
+    case noteBooks of
+        Nothing ->
+            div []
+                [ text "Loading..."
+                ]
+
+        Just noteBooks ->
+            div []
+                [ ul [] (List.map renderNoteBook noteBooks)
+                ]
+
+layout : Model -> Html Msg
+layout model =
+    div [] [ ]
+
 view : Model -> Html Msg
 view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+    div [] [
+        renderNoteBooks model.notebooks
         ]
 
 
