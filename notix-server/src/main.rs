3f194daacd85ccf9f28e373a6f40525577907ede
Pascal Widdershoven <hello@pascalw.me>
2018-07-05 07:29:36 +0200

    Add rust logger

--- notix-server/src/main.rs
+++ notix-server/src/main.rs
@@ -5,14 +5,33 @@ use actix_web::{server, App};
 extern crate rust_embed;
 extern crate mime_guess;
 
+#[macro_use] extern crate log;
+extern crate env_logger;
+
+use std::env;
+
 mod assets;
 
 fn main() {
+    configure_logger();
+
+    let port = port();
+
     server::new(|| {
         App::new()
             .resource("/{path:.*}", |r| r.f(assets::handler))
     })
-    .bind("127.0.0.1:8000")
-    .expect("Can not bind to port 8000")
+    .bind(format!("127.0.0.1:{}", port))
+    .expect(&format!("Can not bind to port {}", port))
     .run();
+}
+
+fn configure_logger() {
+    env_logger::Builder::from_default_env()
+        .filter_level(log::LevelFilter::Info)
+        .init();
+}
+
+fn port() -> String {
+    env::var("PORT").unwrap_or("8000".to_string())
 }
