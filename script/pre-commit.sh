9916724c0def33f44c2b8f89a7907a861d54778b
Pascal Widdershoven <hello@pascalw.me>
2018-07-12 12:52:38 +0200

    Add `cargo check` and `cargo clippy` to pre-commit hook

--- script/pre-commit.sh
+++ script/pre-commit.sh
@@ -1,9 +1,14 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 CHANGED_FILES=$(git diff --cached --name-status --diff-filter=ACM | awk '/\.rs$/ { print $2 }')
 
 [ ! -z "$CHANGED_FILES" ] && {
   echo "Running rustfmt..."
   rustfmt --write-mode=overwrite $CHANGED_FILES
-  cargo +nightly clippy
+
+  echo "Running cargo check..."
+  (cd notex-server && cargo check) || exit 1
+
+  echo "Running clippy..."
+  (cd notex-server && cargo +nightly clippy)
   git add $CHANGED_FILES
 } || exit 0
