91bf0eaee87ce3c63620e6c67bc97b8e469cfa45
Pascal Widdershoven <hello@pascalw.me>
2018-07-12 12:43:39 +0200

    Add clippy to pre-commit hook

--- script/pre-commit.sh
+++ script/pre-commit.sh
@@ -4,5 +4,6 @@ CHANGED_FILES=$(git diff --cached --name-status --diff-filter=ACM | awk '/\.rs$/
 [ ! -z "$CHANGED_FILES" ] && {
   echo "Running rustfmt..."
   rustfmt --write-mode=overwrite $CHANGED_FILES
+  cargo +nightly clippy
   git add $CHANGED_FILES
 } || exit 0
