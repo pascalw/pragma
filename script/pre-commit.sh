0848900c7a9ab23bec392ad1317f7ee6caf989d1
Pascal Widdershoven <hello@pascalw.me>
2018-07-21 19:17:13 +0200

    Add Reason formatting to pre-commit

--- script/pre-commit.sh
+++ script/pre-commit.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
-CHANGED_FILES=$(git diff --cached --name-status --diff-filter=ACM | awk '/\.rs$/ { print $2 }')
+CHANGED_RUST_FILES=$(git diff --cached --name-only --diff-filter=ACM "*.rs" | tr '\n' ' ')
+CHANGED_REASON_FILES=$(git diff --cached --name-only --diff-filter=ACM "*.re" | tr '\n' ' ')
 
-[ ! -z "$CHANGED_FILES" ] && {
+[ ! -z "$CHANGED_RUST_FILES" ] && {
   echo "Running rustfmt..."
-  rustfmt --write-mode=overwrite $CHANGED_FILES
+  rustfmt --write-mode=overwrite "$CHANGED_RUST_FILES"
 
   echo "Running cargo check..."
-  (cd notex-server && cargo check) || exit 1
+  (cd notex-server && cargo check >/dev/null 2>&1) || exit 1
 
   echo "Running clippy..."
   (cd notex-server && cargo +nightly clippy)
-  git add $CHANGED_FILES
+  echo "$CHANGED_RUST_FILES" | xargs git add
+}
+
+[ ! -z "$CHANGED_REASON_FILES" ] && {
+  echo "Running refmt..."
+  echo "$CHANGED_REASON_FILES" | xargs -L1 refmt --in-place
+
+  echo "$CHANGED_REASON_FILES" | xargs git add
 } || exit 0
