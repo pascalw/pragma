fa51e5c170b15678a55912a105e42034d9accb64
Pascal Widdershoven <hello@pascalw.me>
2018-09-22 19:34:21 +0200

    Fix build script

--- script/build.sh
+++ script/build.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 echo "Building frontend..."
-(cd frontend && yarn build)
+(cd notex-web && yarn build)
 
 echo "Copying assets..."
-(cd frontend/build && \
+(cd notex-web/build && \
   tar cf - \
     --exclude=**.map \
-.) | (cd notix-server/assets && tar xvf - )
+.) | (cd notex-server/assets && tar xvf - )
 
 echo "Building binary"
-(cd notix-server && cargo build --release --features=embedded_assets)
+(cd notex-server && cargo build --release --features=embedded_assets)
